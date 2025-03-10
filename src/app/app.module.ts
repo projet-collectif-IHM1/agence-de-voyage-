2b4409d10348c37d8d7a29281d966fc72f551530
yessine-abdennadher <yassineabdennadher22@gmail.com>
2025-03-10 12:52:26 +0100

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,36 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatChipsModule} from '@angular/material/chips';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+import {MatButtonToggleModule} from '@angular/material/button-toggle';
+import { HeaderComponent } from './shared/header/header.component';
+import {MatIconModule} from '@angular/material/icon';
+import { MatTableModule } from '@angular/material/table';
+import { HomeComponent } from './home/home.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+ 
+    HeaderComponent,
+      HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatGridListModule,
+    MatChipsModule,
+    MatProgressSpinnerModule,
+    MatSlideToggleModule,
+    MatButtonToggleModule,
+    MatIconModule,
+    MatTableModule
+ 
+
   ],
   providers: [],
   bootstrap: [AppComponent]
